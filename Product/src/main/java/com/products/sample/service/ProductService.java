afb9ca45de4542c409dc47125c6786773aa23a76
riazvm <riazm@vmware.com>
2019-06-26 00:04:57 -0500

    updated code

--- Product/src/main/java/com/products/sample/service/ProductService.java
+++ Product/src/main/java/com/products/sample/service/ProductService.java
@@ -2,17 +2,8 @@ package com.products.sample.service;
 
 import com.products.sample.model.Product;
 
-
-import java.io.IOException;
-
 public interface ProductService {
-    /**
-     * Generate QR Code
-     *
-     * @param Product
-     *
-     * @return String
-  */
+	
+	public Product createProduct(Product product) throws Exception;
 
-    Product createProduct(Product product) throws Exception;
 }
